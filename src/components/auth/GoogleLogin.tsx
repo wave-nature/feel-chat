198652298d9ba75341f79812d2cbe6f61fc7a08d
wave-nature <vs1010582@gmail.com>
2024-04-14 17:25:17 +0530

    fix: google uri redirect

--- src/components/auth/GoogleLogin.tsx
+++ src/components/auth/GoogleLogin.tsx
@@ -8,6 +8,7 @@ import { BASE_URL } from "@/utils/env";
 const supabase = createClient();
 
 function GoogleLogin() {
+  // console.log(BASE_URL);
   return (
     <Auth
       supabaseClient={supabase}
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,4 @@
 const production = true;
 export const BASE_URL = production
-  ? "https://feelchat.vercel.app"
+  ? "https://feel-chat.vercel.app"
   : "http://localhost:3000";
